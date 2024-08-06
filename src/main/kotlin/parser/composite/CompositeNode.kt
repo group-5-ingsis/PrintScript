78d6b34c5b903b10e23e892c6665b1a8e2fb9242
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-08-06 11:51:48 +0200

    modified Node structure to match the Token's

--- src/main/kotlin/parser/composite/CompositeNode.kt
+++ src/main/kotlin/parser/composite/CompositeNode.kt
@@ -3,20 +3,20 @@ package parser.composite
 import Position
 import token.TokenType
 
-class CompositeNode(private val type: TokenType, private val location: Position) : Node {
-    private val children = mutableListOf<Node>()
+class CompositeNode(private val type: TokenType, private val value: String, private val location: Position) : Node {
+  private val children = mutableListOf<Node>()
 
-    override fun addChild(node: Node) {
-        children.add(node)
-    }
+  override fun addChild(node: Node) {
+    children.add(node)
+  }
 
-    override fun removeChild(node: Node) {
-        children.remove(node)
-    }
+  override fun removeChild(node: Node) {
+    children.remove(node)
+  }
 
-    override fun solve() {
-        for (child: Node in children) {
-            child.solve()
-        }
+  override fun solve() {
+    for (child: Node in children) {
+      child.solve()
     }
+  }
 }
--- src/main/kotlin/parser/composite/Leaf.kt
+++ src/main/kotlin/parser/composite/Leaf.kt
@@ -2,14 +2,20 @@ package parser.composite
 
 import Position
 import token.TokenType
+import kotlin.jvm.Throws
 
-class Leaf(private val type: TokenType, private val location: Position): Node {
+class Leaf(private val type: TokenType, private val value: String, private val location: Position): Node {
 
-    override fun addChild(node: Node) { }
+    override fun addChild(node: Node) {
+      throw IllegalArgumentException("Illegal operation for leaf node")
+    }
 
-    override fun removeChild(node: Node) { }
+    @Throws(IllegalArgumentException::class)
+    override fun removeChild(node: Node) {
+      throw IllegalArgumentException("Illegal operation for leaf node")
+    }
 
     override fun solve() {
-        TODO("Not yet implemented")
+      TODO("Not yet implemented")
     }
 }
