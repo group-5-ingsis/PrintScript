611402974f4d6b1932748b51f201a56ab3b069f6
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-08-07 17:46:37 -0300

    tests added

--- src/test/kotlin/token/TokenTypeTest.kt
+++ src/test/kotlin/token/TokenTypeTest.kt
@@ -5,85 +5,76 @@ import org.junit.jupiter.api.Test
 
 object TokenTypeTest {
 
-//    @Test
-//    fun testTokenTypeLet() {
-//        val string = "let"
-//        val expected = TokenType.KEYWORD
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeConst() {
-//        val string = "const"
-//        val expected = TokenType.KEYWORD
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeString() {
-//        val string = "\"car\""
-//        val expected = TokenType.STRING
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeNumber() {
-//        val string = "1"
-//        val expected = TokenType.NUMBER
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeOperator() {
-//        val string = "+"
-//        val expected = TokenType.OPERATOR
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypePunctuation() {
-//        val string = ";"
-//        val expected = TokenType.PUNCTUATION
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeDeclaration() {
-//        val string = ":"
-//        val expected = TokenType.PUNCTUATION
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeNumberType() {
-//        val string = "number"
-//        val expected = TokenType.VARIABLETYPE
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
-//
-//    @Test
-//    fun testTokenTypeStringType() {
-//        val string = "string"
-//        val expected = TokenType.VARIABLETYPE
-//
-//        val actual = TokenGenerator.getTypeFromValue(string)
-//        assertEquals(expected, actual)
-//    }
+    @Test
+    fun testTokenTypeLet() {
+        val string = "let"
+        val expected = "ASSIGNATION_KEYWORD"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeString() {
+        val string = "\"car\""
+        val expected = "STRING"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeNumber() {
+        val string = "1"
+        val expected = "NUMBER"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeOperator() {
+        val string = "+"
+        val expected = "OPERATOR"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypePunctuation() {
+        val string = ";"
+        val expected = "PUNCTUATION"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeDeclaration() {
+        val string = ":"
+        val expected = "PUNCTUATION"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeNumberType() {
+        val string = "Number"
+        val expected = "VARIABLE_TYPE"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun testTokenTypeStringType() {
+        val string = "String"
+        val expected = "VARIABLE_TYPE"
+
+        val actual = TokenGenerator.getTypeFromValue(string)
+        assertEquals(expected, actual)
+    }
 }
 
