80fe9953264275d79766828c79e53ad22f435035
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-08-10 17:20:07 +0200

    AssignationASTBuilder

---
+++ parser/src/main/kotlin/parser/builders/AssignationASTBuilder.kt
@@ -0,0 +1,12 @@
+package parser.builders
+
+import parser.SyntacticParser
+import parser.composite.Node
+import parser.statement.Statement
+
+class AssignationASTBuilder : ASTBuilder {
+
+  override fun build(statement: Statement, root: SyntacticParser.RootNode): Node {
+    TODO("Not yet implemented")
+  }
+}
