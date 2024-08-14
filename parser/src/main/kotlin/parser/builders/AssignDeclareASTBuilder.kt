b15acf7a57b6f2d966aa5e730b6a43457fb5d42b
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-08-14 11:16:44 +0200

    AssignDeclare AST builder implemented

--- parser/src/main/kotlin/parser/builders/AssignDeclareASTBuilder.kt
+++ parser/src/main/kotlin/parser/builders/AssignDeclareASTBuilder.kt
@@ -2,23 +2,33 @@ package parser.builders
 
 import composite.Node
 import composite.types.*
+import parser.exceptions.UnsupportedLeafTypeException
 import parser.statement.Statement
 import token.Token
 
 class AssignDeclareASTBuilder : ASTBuilder {
 
-  /* ASSIGNDECLARE STRUCTURE: let a: Number = 22; */
+  /* ASSIGNDECLARE STRUCTURE: let a: Number = 22; // "Test"; // b; */
   override fun build(statement: Statement): Node {
     val tokens: List<Token> = statement.content
+    val leafNode: Node = getLeafNodeType(tokens)
     val assignation: Node = Assignation(
       Declaration(
         Identifier(tokens[1].value),
         VariableType(tokens[3].type),
       ),
-      TODO()
-        //Literal()
-        //tokens[5].value
+      leafNode
     )
     return assignation
+  }
+
+  private fun getLeafNodeType(tokens: List<Token>): Node {
+    val token: Token = tokens[5]
+    return when (token.type) {
+      "NUMBER" -> NumericLiteral(token.value.toDouble())
+      "STRING" -> StringLiteral(token.value)
+      "IDENTIFIER" -> Identifier(token.value)
+      else -> throw UnsupportedLeafTypeException("Unexpected leaf type: ${token.type}")
+    }
   }
 }
---
+++ parser/src/main/kotlin/parser/builders/MethodCallASTBuilder.kt
@@ -0,0 +1,11 @@
+package parser.builders
+
+import composite.Node
+import parser.statement.Statement
+
+class MethodCallASTBuilder : ASTBuilder {
+
+  override fun build(statement: Statement): Node {
+    TODO("Not yet implemented")
+  }
+}
