eba34113274883c034ce962813371761fdb6c4b9
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-20 18:26:42 -0300

    delete sca files

--- sca/src/main/kotlin/sca/rules/FunctionArgumentCheck.kt
+++
@@ -1,7 +0,0 @@
-package sca.rules
-
-class FunctionArgumentCheck : AnalyzerRules {
-    override fun applyRule(configFilePath: String): Boolean {
-        return getConfigFileValue(configFilePath, "printlnArgumentCheck") { it.toBoolean() }
-    }
-}
