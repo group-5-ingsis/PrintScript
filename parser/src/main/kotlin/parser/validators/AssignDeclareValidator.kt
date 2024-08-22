6fcf0c11f494014d69694d05e81e58071ef46ecc
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-08-21 23:27:42 -0300

    AssignDeclare Validator almost working

--- parser/src/main/kotlin/parser/validators/AssignDeclareValidator.kt
+++ parser/src/main/kotlin/parser/validators/AssignDeclareValidator.kt
@@ -2,22 +2,20 @@ package parser.validators
 
 import Node
 
-class AssignDeclareValidator {
-  // Structure: const a : Number = 33;
+class AssignDeclareValidator : Validator<Node.AssignationDeclaration> {
   // Node structure: AssignationDeclaration(type, kind, identifier, value(value, type))
   // Validation: Type in Declaration MUST match type on assignation.
-  fun validateSemantics(node: Node.AssignationDeclaration): ValidationResult {
-    val declaredType = node.nodeType
-    val actualType = node.value
-    if (declaredType == actualType.nodeType) {
-      return ValidationResult(false, null, null)
+  override fun validate(node: Node.AssignationDeclaration): ValidationResult {
+    val declaredType = node.dataType.type
+    val actualType = node.value.nodeType
+    return if (declaredType == actualType) {
+      ValidationResult(false, null, null)
     } else {
-      return ValidationResult(
+      ValidationResult(
         true,
         null,
         "Declared type $declaredType does not match assigned type $actualType",
       )
     }
-    TODO("Not yet implemented")
   }
 }
