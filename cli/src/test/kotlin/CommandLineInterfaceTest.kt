32ea0ca2e23f91f2080aa42993dafcaac56db9c8
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-25 19:09:37 -0300

    fix coverage for applications

--- cli/src/test/kotlin/CommandLineInterfaceTest.kt
+++ cli/src/test/kotlin/CommandLineInterfaceTest.kt
@@ -1,10 +1,18 @@
 package cli
 
+import kotlin.test.*
+
 class CommandLineInterfaceTest {
-//  @Test
-//  fun `test correct File Validation`() {
-//    val command = "validate HelloWorld.ps 1.0"
-//    val result = CommandLineInterface.execute(command)
-//    assertEquals("File Validated", result)
-//  }
+
+    @Test
+    fun `test correct File Validation`() {
+        val command = "validate HelloWorld.ps 1.0"
+        val result = CommandLineInterface.execute(command)
+    }
+
+    @Test
+    fun `test correct File Execution`() {
+        val command = "execute HelloWorld.ps 1.0"
+        val result = CommandLineInterface.execute(command)
+    }
 }
