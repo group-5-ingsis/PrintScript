29013add4b5294110c4fbc5a683d461412d14bd5
husseymarcos <mhussey@mail.austral.edu.ar>
2024-08-27 23:21:56 -0300

    tests and binaryOperation files

--- formatter/src/test/kotlin/FormatterTest.kt
+++ formatter/src/test/kotlin/FormatterTest.kt
@@ -37,4 +37,72 @@ class FormatterTest {
         val formattedCode = Formatter.format(child, exampleRules)
         assertEquals(goodFileContents, formattedCode)
     }
+
+    @Test
+    fun `test assignationDeclaration formatting`() {
+        val badFileContents = javaClass.getResource("/assignationDeclaration/badFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val goodFileContents = javaClass.getResource("/assignationDeclaration/goodFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val tokens = Lexer.lex(badFileContents, listOf())
+
+        val ast = parser.run(tokens)
+        val child = ast.getChildren().first()
+
+        val formattedCode = Formatter.format(child, exampleRules)
+        assertEquals(goodFileContents, formattedCode)
+    }
+
+    @Test
+    fun `test declaration formatting`() {
+        val badFileContents = javaClass.getResource("/declaration/badFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val goodFileContents = javaClass.getResource("/declaration/goodFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val tokens = Lexer.lex(badFileContents, listOf())
+
+        val ast = parser.run(tokens)
+        val child = ast.getChildren().first()
+
+        val formattedCode = Formatter.format(child, exampleRules)
+        assertEquals(goodFileContents, formattedCode)
+    }
+
+    @Test
+    fun `test methodCall formatting`() {
+        val badFileContents = javaClass.getResource("/methodCall/badFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val goodFileContents = javaClass.getResource("/methodCall/goodFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val tokens = Lexer.lex(badFileContents, listOf())
+
+        val ast = parser.run(tokens)
+        val child = ast.getChildren().first()
+
+        val formattedCode = Formatter.format(child, exampleRules)
+        assertEquals(goodFileContents, formattedCode)
+    }
+
+    @Test
+    fun `test operator formatting`() {
+        val badFileContents = javaClass.getResource("/binaryOperation/badFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val goodFileContents = javaClass.getResource("/binaryOperation/goodFormat.ps")?.readText()
+            ?: throw IllegalArgumentException("File not found")
+
+        val tokens = Lexer.lex(badFileContents, listOf())
+
+        val ast = parser.run(tokens)
+        val child = ast.getChildren().first()
+
+        val formattedCode = Formatter.format(child, exampleRules)
+        assertEquals(goodFileContents, formattedCode)
+    }
 }
