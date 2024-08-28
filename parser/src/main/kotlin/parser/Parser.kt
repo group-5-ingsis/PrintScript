911bd7d4150246e4b203a87538d8a98f1a3b006a
Carla Boggio <carlaboggio123@gmail.com>
2024-08-28 15:37:23 -0300

    add parser class

--- parser/src/main/kotlin/parser/Parser.kt
+++ parser/src/main/kotlin/parser/Parser.kt
@@ -1,14 +1,33 @@
 package parser
 
+import Environment
 import SemanticErrorException
+import parser.validation.SemanticValidator
 import token.Token
+import visitor.NodeVisitor
 
 
 // Singleton object since it can be reused with different arguments.
 class Parser {
+    private val syntacticParser = SemanticValidator()
+
+
+
 
     @Throws(SemanticErrorException::class)
     fun run(tokens: List<Token>): SyntacticParser.RootNode {
-        return SyntacticParser(tokens).parse()
+
+        val ast = SyntacticParser(tokens).parse()
+        val environment = Environment()
+        ast.accept(NodeVisitor(environment))
+        val validation = syntacticParser.validateSemantics(ast, environment)
+        println("The validation was: " + validation.isInvalid)
+        return ast
+
+
     }
+
+
+
+
 }
