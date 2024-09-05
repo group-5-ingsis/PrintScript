ad7dc0537861c471b61149d757fe86fa24d06ec7
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-05 14:45:53 -0300

    feat: add rules + setup Test

--- formatter/src/main/kotlin/formatter/Formatter.kt
+++ formatter/src/main/kotlin/formatter/Formatter.kt
@@ -1,22 +1,23 @@
 package formatter
 
 import parser.syntactic.SyntacticParser
+import rules.FormattingRules
 
 class Formatter(private val parser: Iterator<SyntacticParser.RootNode>) {
 
-    fun format(): String {
+    fun format(rules: FormattingRules): String {
         val formattedOutput = StringBuilder()
 
         while (parser.hasNext()) {
             val astNode = parser.next()
-            val formattedNode = formatASTNode(astNode)
+            val formattedNode = formatASTNode(astNode, rules)
             formattedOutput.append(formattedNode)
         }
 
         return formattedOutput.toString()
     }
 
-    private fun formatASTNode(node: SyntacticParser.RootNode): String {
-        return NodeFormatter.format(node)
+    private fun formatASTNode(node: SyntacticParser.RootNode, rules: FormattingRules): String {
+        return NodeFormatter.format(node, rules)
     }
 }
--- formatter/src/test/kotlin/FormatterTest.kt
+++ formatter/src/test/kotlin/FormatterTest.kt
@@ -1,114 +1,36 @@
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
 import formatter.Formatter
-import lexer.Lexer
-import parser.syntactic.SyntacticParser
+import parser.Parser
 import rules.FormattingRules
 import java.nio.file.Paths
 import kotlin.test.BeforeTest
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class FormatterTest {
 
     private lateinit var exampleRules: FormattingRules
-    private val parser = SyntacticParser()
 
     @BeforeTest
     fun setUp() {
         val yamlMapper = YAMLMapper()
 
-        // Load the file from the classpath
         val resource = this::class.java.getResource("/rules/testRules.yaml")
             ?: throw IllegalArgumentException("File not found!")
         val file = Paths.get(resource.toURI()).toFile()
 
-        // Map the YAML content to FormattingRules class
         exampleRules = yamlMapper.readValue(file, FormattingRules::class.java)
     }
 
     @Test
-    fun `test format`() {
-        val badFileContents = javaClass.getResource("/assignation/badFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
+    fun testFormatting() {
+        val input = "let a: Number = 2; a = 4; println(a)"
 
-        val goodFileContents = javaClass.getResource("/assignation/goodFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
+        val lexer = Lexer(input)
+        val parser = Parser(lexer)
+        val formatter = Formatter(parser)
 
-        val tokens = Lexer.lex(badFileContents)
+        val result = formatter.format(exampleRules)
 
-        val ast = parser.run(tokens)
-        val child = ast.getChildren().first()
-
-        val formattedCode = Formatter.format(child, exampleRules)
-
-        assertEquals(goodFileContents, formattedCode)
-    }
-
-    @Test
-    fun `test assignationDeclaration formatting`() {
-        val badFileContents = javaClass.getResource("/assignationDeclaration/badFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val goodFileContents = javaClass.getResource("/assignationDeclaration/goodFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val tokens = Lexer.lex(badFileContents)
-
-        val ast = parser.run(tokens)
-        val child = ast.getChildren().first()
-
-        val formattedCode = Formatter.format(child, exampleRules)
-        assertEquals(goodFileContents, formattedCode)
-    }
-
-    @Test
-    fun `test declaration formatting`() {
-        val badFileContents = javaClass.getResource("/declaration/badFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val goodFileContents = javaClass.getResource("/declaration/goodFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val tokens = Lexer.lex(badFileContents)
-
-        val ast = parser.run(tokens)
-        val child = ast.getChildren().first()
-
-        val formattedCode = Formatter.format(child, exampleRules)
-        assertEquals(goodFileContents, formattedCode)
-    }
-
-    @Test
-    fun `test methodCall formatting`() {
-        val badFileContents = javaClass.getResource("/methodCall/badFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val goodFileContents = javaClass.getResource("/methodCall/goodFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val tokens = Lexer.lex(badFileContents)
-
-        val ast = parser.run(tokens)
-        val child = ast.getChildren().first()
-
-        val formattedCode = Formatter.format(child, exampleRules)
-        assertEquals(goodFileContents, formattedCode)
-    }
-
-    @Test
-    fun `test operator formatting`() {
-        val badFileContents = javaClass.getResource("/binaryOperation/badFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val goodFileContents = javaClass.getResource("/binaryOperation/goodFormat.ps")?.readText()
-            ?: throw IllegalArgumentException("File not found")
-
-        val tokens = Lexer.lex(badFileContents)
-
-        val ast = parser.run(tokens)
-        val child = ast.getChildren().first()
-
-        val formattedCode = Formatter.format(child, exampleRules)
-        assertEquals(goodFileContents, formattedCode)
+        println(result)
     }
 }
