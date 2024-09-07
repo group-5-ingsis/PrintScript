a51a4a6229c8f3291e04f379db5293ca83f0a086
lucasmonteverdi1 <128722688+lucasmonteverdi1@users.noreply.github.com>
2024-09-07 20:55:18 -0300

    tests + access modifiers on methods

--- parser/src/test/kotlin/parser/SemanticTesting.kt
+++ parser/src/test/kotlin/parser/SemanticTesting.kt
@@ -7,7 +7,7 @@ class SemanticTesting {
 
     @Test
     fun testDeclarationShouldPass() {
-        val lexer = Lexer("let a: String;")
+        val lexer = Lexer("let a: String")
         val parser = Parser(lexer)
         println(parser.next())
     }
@@ -15,25 +15,52 @@ class SemanticTesting {
     @Test
     fun testDeclarationShouldFail() {
         /* Declaration of const. */
+        val lexer = Lexer("const a: String;")
+        val parser = Parser(lexer)
+        println(parser.next())
     }
 
     @Test
     fun testAssignationShouldPass() {
+        val lexer = Lexer("let a: String; a = \"testing\";")
+        val parser = Parser(lexer)
+        println(parser.next())
     }
 
     @Test
     fun testAssignationToDifferentTypeShouldFail() {
+        val lexer = Lexer("let a: String; a = 22;")
+        val parser = Parser(lexer)
     }
 
     @Test
     fun testAssignationToConstShouldFail() {
+        val lexer = Lexer("const a: String = \"test\"; a = \"testing\";")
+        val parser = Parser(lexer)
     }
 
     @Test
     fun testAssignationToInexistentVariableShouldFail() {
+        val lexer = Lexer("let a: String; a = b;")
+        val parser = Parser(lexer)
+    }
+
+    @Test
+    fun testAssignationOfInexistentVariableShouldFail() {
+        val lexer = Lexer("b = 23;")
+        val parser = Parser(lexer)
     }
 
     @Test
     fun testAssignDeclareShouldPass() {
+        val lexer = Lexer("let b: Number = 23;")
+        val parser = Parser(lexer)
+        println(parser.next())
+    }
+
+    @Test
+    fun testAssignDeclareShouldFail() {
+        val lexer = Lexer("let b: String = 23;")
+        val parser = Parser(lexer)
     }
 }
