8495bcfa30832bc743fcfae183f19de4f6e9d0a0
Mateo Piccinini <128090505+mPiccinini@users.noreply.github.com>
2024-09-10 21:51:06 -0300

    Linter

--- linter/src/main/kotlin/rules/LinterRulesV1.kt
+++ linter/src/main/kotlin/rules/LinterRulesV1.kt
@@ -1,12 +1,6 @@
 package rules
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
-
-data class LinterRulesV1 @JsonCreator constructor(
-    @JsonProperty("identifierNamingConvention") val identifierNamingConvention: String = "off",
-    @JsonProperty("printlnExpressionAllowed") val printlnExpressionAllowed: Boolean = true
-) : LinterRules {
+data class LinterRulesV1(private val identifierNamingConvention: String = "off", val printlnExpressionAllowed: Boolean = true) : LinterRules {
     override fun getAsMap(): Map<String, Any> {
         return mapOf(
             "identifierNamingConvention" to identifierNamingConvention,
--- linter/src/test/kotlin/LinterTests.kt
+++ linter/src/test/kotlin/LinterTests.kt
@@ -39,7 +39,7 @@ class LinterTests {
 
     @Test
     fun testPrintlnCallValidWithIdentifier() {
-        val input = "let a : String = 'hello' ;println(a);"
+        val input = "let a : string = 'hello' ;println(a);"
         val tokens = Lexer(input, version)
         val asts = Parser(tokens, version)
         val rules = LinterRulesV1("camel-case", false)
@@ -51,7 +51,7 @@ class LinterTests {
 
     @Test
     fun testDeclarationOffValid() {
-        val input = "let aA : String = 'hello'; let a_b : String = 'hello';"
+        val input = "let aA : string = 'hello'; let a_b : string = 'hello';"
         val tokens = Lexer(input, version)
         val asts = Parser(tokens, version)
         val rules = LinterRulesV1("off", false)
@@ -63,7 +63,7 @@ class LinterTests {
 
     @Test
     fun testDeclarationSnake_Case() {
-        val input = "let aA : String = 'hello'; let a_b : String = 'hello';"
+        val input = "let aA : string = 'hello'; let a_b : String = 'hello';"
         val tokens = Lexer(input, version)
         val asts = Parser(tokens, version)
         val rules = LinterRulesV1("snake-case", false)
@@ -79,7 +79,7 @@ class LinterTests {
 
     @Test
     fun testDeclarationCamelCase() {
-        val input = "let aA : String = 'hello'; let a_b : String = 'hello';"
+        val input = "let aA : string = 'hello'; let a_b : String = 'hello';"
         val tokens = Lexer(input, version)
         val asts = Parser(tokens, version)
         val rules = LinterRulesV1("camel-case", false)
