938faaf0c7dc3831173a563e957fe8078744debe
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-22 18:45:18 -0300

    fix: CLI tests assert order

--- cli/src/test/kotlin/CommandLineInterfaceTest.kt
+++ cli/src/test/kotlin/CommandLineInterfaceTest.kt
@@ -8,14 +8,14 @@ class CommandLineInterfaceTest {
     fun `test correct File Validation`() {
         val command = "validate HelloWorld.ps 1.0"
         val result = CommandLineInterface.execute(command)
-        assertEquals(result, "File Validated! (No Errors found)")
+        assertEquals("File Validated! (No Errors found)", result)
     }
 
     @Test
     fun `test correct File Execution`() {
         val command = "execute HelloWorld.ps 1.0"
         val result = CommandLineInterface.execute(command)
-        assertEquals(result, "\nFinished executing HelloWorld.ps")
+        assertEquals("\nFinished executing HelloWorld.ps", result)
     }
 
     @Test
