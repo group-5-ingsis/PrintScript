ba02d422df3326199f9ba2d2e6d05c84b61dc79d
husseymarcos <mhussey@mail.austral.edu.ar>
2024-09-27 12:27:24 -0300

    refactor: remove some methods in Parser

--- parser/src/main/kotlin/parser/Parser.kt
+++ parser/src/main/kotlin/parser/Parser.kt
@@ -11,40 +11,25 @@ import visitor.PrintScriptInputProvider
 class Parser(
     private val lexer: Iterator<Token>,
     private val version: String = "1.1",
-    private var readInput: InputProvider = PrintScriptInputProvider()
+    private var readInput: InputProvider = PrintScriptInputProvider(),
+    private var env: Environment = Environment()
 ) : Iterator<Statement> {
-    private var env = Environment()
-    private val momentList: ArrayDeque<Token> = ArrayDeque()
+
+    var tokens: List<Token> = listOf()
 
     override fun hasNext(): Boolean {
         return lexer.hasNext()
     }
 
-    fun setEnv(env: Environment): Environment {
-        this.env = env
-        return env
-    }
-
-    fun getEnv(): Environment {
-        return env
-    }
-
     override fun next(): Statement {
-        var tokensForParsing: List<Token> = momentList.toList()
         var lastException: Exception? = null
 
-        while (lexer.hasNext() || momentList.isNotEmpty()) {
-            if (momentList.isNotEmpty()) {
-                val firstToken = momentList.removeFirst()
-                tokensForParsing = tokensForParsing + firstToken
-            } else {
-                val nextToken = lexer.next()
-                tokensForParsing = tokensForParsing + nextToken
-            }
+        while (lexer.hasNext()) {
+            val nextToken = lexer.next()
+            tokens = tokens + nextToken
 
             try {
-                val statement = SyntacticParser.parse(tokensForParsing, version)
-                ifChecker()
+                val statement = SyntacticParser.parse(tokens, version)
 
                 env = SemanticParser.validate(statement, env, readInput)
 
@@ -66,18 +51,9 @@ class Parser(
         throw NoSuchElementException("No more tokens available to parse")
     }
 
-    private fun ifChecker() {
-        val hasNext = lexer.hasNext()
-        if (hasNext) {
-            val next = lexer.next()
-            momentList.add(next)
-            if (next.type == "ELSE") {
-                throw Exception("Exception for continue the loop")
-            }
-        }
-    }
-
     private fun allowedException(e: Exception): Boolean {
+        val exception = e.message
+
         val allowedExceptions = listOf(
             "No tokens to get position from.",
             "Find unknown expression at line: 0 and at index: 0",
@@ -86,7 +62,6 @@ class Parser(
             "Expect this type: RIGHT_BRACE in Line 0, symbol 0"
         )
 
-        val exception = e.message
         return exception in allowedExceptions
     }
 }
